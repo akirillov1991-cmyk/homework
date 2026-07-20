015bf43a615b9cf09550ff9ce19b026aad51241f
akirillov1991-cmyk <akirillov1991@gmail.com>
2026-07-20 22:11:33 +0300

    homework-js-9

---
+++ homework-js-9/main.js
@@ -0,0 +1,28 @@
+// Задача 1
+
+const FIRST_NAME = "Alexander";
+const LAST_NAME = "Kirillov";
+const IS_STUDENT = true;
+
+// Задача 2
+
+const AGE = 35;
+let currentYear = 2026;
+let birthYear = currentYear - AGE;
+
+// Задача 3
+
+let str = `Меня зовут ${FIRST_NAME} ${LAST_NAME}, мне ${AGE} лет. Я ученик курса: ${IS_STUDENT}`
+
+console.log(str);
+
+// Задача 4
+
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+
+// 123456789falsetrue
